6ce59269552a9c47c9ed863663ec3664ecfdce69
juandolce <juandolce20@gmail.com>
2022-12-28 13:40:14 -0300

    basic design

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from 'react';
+import {
+    FaSignInAlt,
+    FaSignOutAlt,
+    FaUser
+} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 
 export const Navbar = () => {
+
+    const onLogout = () => {
+
+    };
+
     return (
-        <div>Navbar</div>
+        <div className='navbar'>
+            <div className='logo'>
+                <Link to='/'>Intro</Link>
+            </div>
+            <ul>
+                <li>
+                    <button className='btn' onClick={onLogout}>
+                        <FaSignOutAlt /> Logout
+                    </button>
+                </li>
+                <li>
+                    <Link to='./login'>
+                        <FaSignInAlt /> Login
+                    </Link>
+                </li>
+                <li>
+                    <Link to='./register'>
+                        <FaUser /> Register
+                    </Link>
+                </li>
+            </ul>
+        </div>
     )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaUser } from 'react-icons/fa';
 
 export const Register = () => {
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
+        passwordCheck: ""
+    });
+
+    const { username, email, password, passwordCheck } = formData;
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <div>Register</div>
+        <>
+            <section>
+                <h1>
+                    <FaUser /> Register
+                </h1>
+                <p>Create an account</p>
+            </section>
+            <section>
+                <form className='form'>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            id='username'
+                            name='username'
+                            value={username}
+                            className='form-control'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="email"
+                            id='email'
+                            name='email'
+                            value={email}
+                            className='form-control'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            id='password'
+                            name='password'
+                            value={password}
+                            className='form-control'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            id='passwordCheck'
+                            name='passwordCheck'
+                            value={passwordCheck}
+                            className='form-control'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <button type='submit' className='btn btn-block' onSubmit={onSubmit}>Submit</button>
+                    </div>
+                </form>
+            </section>
+        </>
     )
 }
