28780bd89cb4bc95427e8accc1772fc358329aa6
juandolce <juandolce20@gmail.com>
2022-12-28 10:10:23 -0300

    controller edit + client initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './app.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import { Intro } from './pages/Intro';
+import { Register } from './pages/Register';
+import { Login } from './pages/Login';
 
 function App() {
   return (
-    <div className="wrapper">
-      <h1>Caso Base</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Intro />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   )
 }
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -36,7 +36,7 @@ const register = async (req, res) => {
 
             res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true })
             console.log("user", JSON.stringify(user, null, 2));
-            console.log(token);
+            console.log(`token: ${token}`);
 
             // send user details
             return res.status(201).send(user);
