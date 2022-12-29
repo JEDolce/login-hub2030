51cf7fd81cdd7ddfcd6b817776e02bf4c56d5703
juandolce <juandolce20@gmail.com>
2022-12-29 11:58:36 -0300

    Add ToastContainer

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {
   Routes,
   Route
 } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import { Intro } from './pages/Intro';
 import { Register } from './pages/Register';
 import { Login } from './pages/Login';
@@ -20,6 +21,7 @@ function App() {
             <Route path='/register' element={<Register />} />
             <Route path='/login' element={<Login />} />
           </Routes>
+          <ToastContainer />
         </div>
       </BrowserRouter>
     </>
